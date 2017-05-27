431d924f3ef19c4f4f3f3392d25ac1d9b5256ab4
Ryo Takashina <tksnry9@gmail.com>
2017-05-27 14:54:58 +0900

    Initial Commit

---
+++ Assets/SimpleMoveScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMoveScript : MonoBehaviour {
+  
+   public    float speed=0.5f;
+   
+   // Use this for initialization
+   void Start () {
+   
+   }
+   
+   // Update is called once per frame
+   void Update () {
+       transform.Translate(Input.GetAxis("Horizontal")*speed,0,Input.GetAxis("Vertical")*speed);
+   }
+}
